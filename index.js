a735a643fded8ce9dc25ebbdab793294ad41c7d8
badoodee95 <monzoncaleb@yahoo.com>
2023-04-05 20:22:20 -0700

    Add: card images, index.html, index.js, style.css, buttons,  and a basic framework for the blackjack game.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Declaring all the variables for the dealer's cards
+let card1Dealer = document.getElementsByClassName('card-1-dealer');
+let card2Dealer = document.getElementsByClassName('card-2-dealer');
+let card3Dealer = document.getElementsByClassName('card-3-dealer');
+let card4Dealer = document.getElementsByClassName('card-4-dealer');
+let card5Dealer = document.getElementsByClassName('card-5-dealer');
+let card6Dealer = document.getElementsByClassName('card-6-dealer');
+
+// Declaring all the variables for the dealer's cards
+let card1Player = document.getElementsByClassName('card-1-player');
+let card2Player = document.getElementsByClassName('card-2-player');
+let card3Player = document.getElementsByClassName('card-3-player');
+let card4Player = document.getElementsByClassName('card-4-player');
+let card5Player = document.getElementsByClassName('card-5-player');
+let card6Player = document.getElementsByClassName('card-6-player');
+
+/* Creating an array for the cards in order to add them to both the dealer's
+ and player's hand */
+let cardDeck = [
+    
+];
+
+
+
