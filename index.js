1864f298e65f04e026f121c98062c9d086733d87
badoodee95 <monzoncaleb@yahoo.com>
2023-04-10 11:27:05 -0700

    feat: finished with hit button and stand button

--- index.js
+++ index.js
@@ -2,29 +2,32 @@
     // Declaring all the variables for the dealer's cards
     let card1Dealer = document.querySelector('#card-1-dealer');
     let card2Dealer = document.querySelector('#card-2-dealer');
+    let newSecondCardDealer = card2Dealer;
     let dealerHand = document.querySelectorAll('.dealer-hand');
+    let dealerSide = document.querySelector('.dealer-side');
+    let dealerScore = document.querySelector('#dealer-score');
     // Declaring all the variables for the player's cards
     let card1Player = document.querySelector('#card-1-player');
     let card2Player = document.querySelector('#card-2-player');
     let playerHand = document.querySelectorAll('.player-hand');
-    let playerHandArray = Array.from(playerHand);
-    console.log(playerHandArray)
-    // Empty space for each card slot
-    let emptySpace = document.createElement('img');
-    emptySpace.setAttribute('src', "");
+    let playerSide = document.querySelector('.player-side');
+    let playerScore = document.querySelector('#player-score');
+    let newFirstCardPlayer;
+    let newSecondCardPlayer;
     // Declaring the totals
     let playerTotal = 0;
     let dealerTotal = 0;
-    // Declaring the Aces; needed to declare so that we can track whether the Ace's value can be +1 or +11;
-    let playerAce = 0;
-    let dealerAce = 0;
-    // Declaring the hidden card for the dealer
-    let hidden;
+    // Declaring the Ace; needed to declare so that we can track whether the Ace's value can be +1 or +11;
+    let aceValue;
     // Declaring the deck
     let deck;
-    // Declaring the images for the backs of the cards
+    // Declaring the results text to display on-screen
+    let resultsText = document.querySelector('.results-text');
+    resultsText.textContent = 'TESTING';
+    // Declaring the hidden card for the dealer
+    let hidden;
 
-
+    //CONSTANTLY UPDATE YOUR JAVASCRIPT CODE
 
 
     // ==================================== DECK BUILDING =========================================//
@@ -39,55 +42,86 @@
                 deck.push(cardValues[j] + "_of_" + cardSuits[i]);
             }
         }
+        return deck;
     };
-    buildDeck();
+    deck = buildDeck();
+    console.log(deck);
 
-    // Drawing a random card to add onto the hand
+    // Drawing a random card to add onto the hand ===============================================
     function drawCard() {
         let randomCard = Math.floor(Math.random() * deck.length);
         // console.log(deck[randomCard]);
-
-        let cardImage = document.createElement('img');
-        cardImage.setAttribute('src', "card-images/" + deck[randomCard] + ".svg");
-        let drawnCard = deck[randomCard];
-        deck.splice(randomCard, 1); // Used to remove the card drawn from the deck/array
-        return drawnCard;
+        let card = document.createElement('img');
+        card.setAttribute('src', "card-images/" + deck[randomCard] + ".svg");
+        card = deck[randomCard];
+        deck.splice(card, 1); // Used to remove the card drawn from the deck/array
+        return card;
     }
-
     // =============================== Assigning a value to each card =========================== //
-    function getValue(card) {
-        let data = card.split("_");
-        let value = data[0];
-
-        if (isNaN(value)) {
-            if (value === 'ace') {
-                return 11;
-            } else {
-                return 10;
-            }
+    function getValue(number) {
+        let newArray = number.split("_");
+        let cardValue = newArray[0];
+        if (cardValue === "ace") {
+             aceValue = 11;
+             return cardValue = aceValue;
+        } else if(cardValue === "king" || cardValue === "queen" || cardValue === "jack") {
+            return 10;
         }
-        return parseInt(value);
-    }
+        return parseInt(cardValue);
+        
+    };
+    console.log(getValue(deck[19]));
 
 
     // ================================ KEEPING TRACK OF THE TOTALS ============================ //
-
+    function keepTrack() {
+        // playerTotal = getValue(card1Player) + getValue(card2Player);
+        let score = document.querySelector('#player-score');
+        playerHand = document.querySelectorAll('.player-hand');
+        playerSide = document.querySelector('.player-side');
+        for (let i = 0; i < playerSide.length; i++) {
+            
+            let playerSideArray = [...playerSide];
+            console.log('hand array', playerSideArray);
+        }
+        score.textContent = "Player: " + playerTotal;
+    }
+    
 
 
 
     // ======================================= PLAY BUTTON ============================== //
     let playButton = document.querySelector('#play');
     playButton.addEventListener('click', function() {
-        let newFirstCardPlayer = card1Player;
-        newFirstCardPlayer.setAttribute('src', "card-images/" + drawCard() + ".svg");
-        let newSecondCardPlayer = card2Player;
-        newSecondCardPlayer.setAttribute('src', "card-images/" + drawCard() + ".svg");
-        let newFirstCardDealer = card1Dealer;
-        newFirstCardDealer.setAttribute('src', "card-images/" + drawCard() + ".svg");
-        let newSecondCardDealer = card2Dealer;
-        newSecondCardDealer.style.height = "325px";
+        hitButton.addEventListener('click', buttonHit);
+        standButton.addEventListener('click', buttonStand);
+        resultsText = document.querySelector('.results-text');
+        resultsText.textContent = 'GAME BEGINS'
+        newFirstCardPlayer = drawCard();
+        // console.log('new first card', newFirstCardPlayer);
+        card1Player.setAttribute('src', "card-images/" + newFirstCardPlayer + ".svg");
+        newSecondCardPlayer = drawCard();
+        // console.log('new second card', newSecondCardPlayer);
+        card2Player.setAttribute('src', "card-images/" + newSecondCardPlayer + ".svg");
+        let newFirstCardDealer = drawCard();
+        card1Dealer.setAttribute('src', "card-images/" + newFirstCardDealer + ".svg");
+        dealerTotal += getValue(newFirstCardDealer);
+        card2Dealer.style.height = "325px";
+        hidden = drawCard();
+        addHiddenCardtoTotal(); 
+        card2Dealer = newSecondCardDealer;
+        console.log('dealer 2nd card', card2Dealer)
+        console.log('dealer total', dealerTotal)
         playButton.replaceWith(restartButton);
-
+        playerTotal += getValue(newFirstCardPlayer);
+        checkAceTotalPlayer();
+        playerTotal += getValue(newSecondCardPlayer);
+        playerScore.textContent = "Player: " + playerTotal;
+        checkPlayerInitialTotal();
+        // console.log("Players first card", newFirstCardPlayer);
+        console.log('playerTotal', playerTotal);
+        blackjackPush();
+        
     });
 
     // ======================================== RESTART BUTTON ======================== //
@@ -99,26 +133,179 @@
         card2Player.setAttribute('src', "card-images/card-back-red.svg");
         card1Dealer.setAttribute('src', "card-images/card-back-red.svg");
         card2Dealer.setAttribute('src', "card-images/card-back-red.svg");
-        while(playerHand.length > 3) {
-            playerHand.pop().remove();
-        }
+        playerHand = document.querySelectorAll('.player-hand');
+        playerSide = document.querySelector('.player-side');
+        console.log("player's hand", playerHand, playerHand.length);
+        while(playerHand.length > 2) {
+            let lastChild = playerSide.lastChild;
+            console.log('last child', lastChild);
+            playerSide.removeChild(lastChild);
+            playerSide = document.querySelector('.player-side');
+            playerHand = document.querySelectorAll('.player-hand');
+        };
+        dealerHand = document.querySelectorAll('.dealer-hand');
+        dealerSide = document.querySelector('.dealer-side');
+        console.log("dealer's hand", dealerHand, dealerHand.length);
+        while(dealerHand.length > 2) {
+            let lastChild = dealerSide.lastChild;
+            console.log('last child dealer', lastChild);
+            dealerSide.removeChild(lastChild);
+            dealerSide = document.querySelector('.dealer-side');
+            dealerHand = document.querySelectorAll('.dealer-hand');
+        };
+        console.log("dealer's side", dealerSide);
+        console.log("player's side", playerSide);
         restartButton.replaceWith(playButton);
         buildDeck();
         card2Dealer.style.height = '';
-
+        playerTotal = 0;
+        dealerTotal = 0;
+        playerScore.textContent = 'Player: ';
+        dealerScore.textContent = 'Dealer: ';
+        resultsText.textContent = 'RESTARTED GAME!';
+        hitButton.removeEventListener('click', buttonHit);
+        standButton.removeEventListener('click', buttonStand);
+        
     })
 
     // ======================================== HIT BUTTON ============================ //
     let hitButton = document.querySelector('#hit');
-    hitButton.addEventListener('click', function() {
-        let newCard = document.createElement('img');
-        newCard.setAttribute('src', "card-images/" + drawCard() + ".svg");
-        playerHand.appendChild(newCard);
-        playerHandArray.appendChild(newCard);
-    })
+    function buttonHit() {
+        let newCard = drawCard();
+        let newCardImg = document.createElement('img');
+        newCardImg.setAttribute('class', 'player-hand');
+        newCardImg.setAttribute('src', "card-images/" + newCard + ".svg");
+        playerSide.append(newCardImg);
+        checkAceTotalPlayer(newCard);
+        playerTotal += getValue(newCard);
+        playerScore.textContent = "Player: " + playerTotal;
+        // console.log("player's hand", playerHand, playerHand.length);
+        playerHand = document.querySelectorAll('.player-hand');
+        playerSide = document.querySelector('.player-side');
+        bust();
+    }
+
+    hitButton.addEventListener('click', buttonHit);
+
+    
+    // ======================================== STAND BUTTON =========================== //
+    let standButton = document.querySelector('#stand');
+    function buttonStand() {
+        card2Dealer = document.querySelector('#card-2-dealer');
+        card2Dealer.setAttribute('src', 'card-images/' + hidden + ".svg");
+        dealerScore = document.querySelector('#dealer-score');
+        dealerScore.textContent = 'Dealer: ' + dealerTotal;
+        dealerAI();
+        resultMessage();
+    }
+
+    standButton.addEventListener('click', buttonStand);
+
+
+
+    // ======================================== SPLIT BUTTON =========================== //
+
+
+    // ======================================== FUNCTION ================================= //
+    function resultMessage() {
+        resultsText = document.querySelector('.results-text');
+        if (playerTotal > 21) {
+            resultsText.textContent = "PLAYER BUST! DEALER WINS!";
+        } else if (dealerTotal > 21) {
+            resultsText.textContent = "DEALER BUST! PLAYER WINS!"
+        } else if (dealerTotal < playerTotal) {
+            resultsText.textContent = "DEALER HOLDS... \n DEALER: " + dealerTotal + " PLAYER: " + playerTotal + " PLAYER WINS!";
+        } else if (playerTotal < dealerTotal) {
+            resultsText.textContent = "PLAYER HOLDS... \n PLAYER: " + playerTotal + " DEALER: " + dealerTotal + " DEALER WINS!";
+        } else if (playerTotal === dealerTotal) {
+            resultsText.textContent = "PUSH..." + " PLAYER: " + playerTotal + " DEALER: " + dealerTotal + " NO WINNER.";
+        }
+    }
+
+    function addHiddenCardtoTotal() {
+        card2Dealer = document.querySelector('#card-2-dealer');
+        card2Dealer = getValue(hidden);
+        dealerTotal += card2Dealer;
+        dealerScore = document.querySelector('#dealer-score');
+        dealerScore.textContent = 'Dealer: ???';
+        if (dealerTotal === 21 && playerTotal !== 21) {
+            dealerScore.textContent = 'Dealer: ' + dealerTotal;
+            resultsText = document.querySelector('.results-text');
+            resultsText.textContent = 'DEALER HAS BLACKJACK! DEALER WINS!'
+            card2Dealer = newSecondCardDealer;
+            card2Dealer.setAttribute('src', "card-images/" + hidden + ".svg")
+            hitButton.removeEventListener('click', buttonHit);
+            standButton.removeEventListener('click', buttonStand);
+        }
+
+    }
+    
+    function bust() {    
+        if (playerTotal > 21 || dealerTotal > 21) {
+            hitButton.removeEventListener('click', buttonHit);
+            standButton.removeEventListener('click', buttonStand);
+            resultMessage();
+            }
+        };
+    
+    
+
+    function checkPlayerInitialTotal() {
+        playerScore = document.querySelector('#player-score');
+        if (playerTotal === 21 && dealerTotal !== 21) {
+            resultsText = document.querySelector('.results-text');
+            resultsText.textContent = 'PLAYER HAS BLACKJACK! PLAYER WINS!'
+            hitButton.removeEventListener('click', buttonHit);
+            standButton.removeEventListener('click', buttonStand);
+        } 
+    }
+
+    function checkAceTotalPlayer(Card) {
+        if (newSecondCardPlayer.split('_')[0] === 'ace' && newSecondCardPlayer.split('_')[0] === 'ace') {
+            playerTotal += 
+            playerScore.textContent = 'Player: ' + playerTotal;
+        }
+        if (newFirstCardPlayer + newSecondCardPlayer + Card > 21) {
+            playerTotal += 1;
+        }
+    };
+
+    function chechAceTotalDealer() {
+        
+    };
+
+    function dealerAI() {
+        dealerTotal;
+        while (dealerTotal < 17) {
+            buttonHitDealer();
+        }
+    }
+
+    function buttonHitDealer() {
+        let newCard = drawCard();
+        let newCardImg = document.createElement('img');
+        newCardImg.setAttribute('class', 'dealer-hand');
+        newCardImg.setAttribute('src', "card-images/" + newCard + ".svg");
+        dealerSide.append(newCardImg);
+        dealerTotal += getValue(newCard);
+        dealerScore.textContent = "Dealer: " + dealerTotal;
+        // console.log("player's hand", playerHand, playerHand.length);
+        dealerHand = document.querySelectorAll('.dealer-hand');
+        dealerSide = document.querySelector('.dealer-side');
+        bust();
+    }
+
+    function blackjackPush() {
+        if (playerTotal === 21 && dealerTotal === 21) {
+            hitButton.removeEventListener('click', buttonHit);
+            standButton.removeEventListener('click', buttonStand);
+            resultsText.querySelector('.results-text');
+            resultsText.textContent = 'BOTH PLAYER AND DEALER HAS BLACKJACK! \n PUSH.'
+        }  
+    }
 
 
 
 
-
-
+    hitButton.removeEventListener('click', buttonHit);
+    standButton.removeEventListener('click', buttonStand);
